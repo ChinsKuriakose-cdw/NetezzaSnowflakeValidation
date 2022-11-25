fe72ccf58968695c3f2dca7fdef7978cb096b697
ChinsKuriakose-cdw <chins.kuriakose@cdw.com>
2022-11-25 23:48:08 +0530

    Changes: 1.added exception, 2.added count validation, 3.updated data validation code.

--- README.md
+++ README.md
@@ -27,13 +27,13 @@ Sample dataframe created by querying the Netezza _v_relation_column view for a p
 Sample dataframe created after running validation queries in Netezza:
 
 ```
-           ATTNAME            FORMAT_TYPE           AVG  MIN        MAX         SUM  NULL_SUM  COUNT
-0     ADD_TYPE_SID                INTEGER  4.452741e+07    0  300180001  1202240056         0     27
-1         ADD_TYPE  CHARACTER VARYING(20)  0.000000e+00  APP        XFR           0         0     27
-2    ADD_TYPE_DESC  CHARACTER VARYING(50)  0.000000e+00  APP        XFR           0         0     27
-3    ETL_LOAD_DATE              TIMESTAMP  0.000000e+00    0          0           0         0      0
-4  ETL_UPDATE_DATE              TIMESTAMP  0.000000e+00    0          0           0         0      0
-5      ETL_USER_ID  CHARACTER VARYING(20)  0.000000e+00  APP        XFR           0         0     27
-6          CHANNEL  CHARACTER VARYING(50)  0.000000e+00  APP        XFR           0         0     27
-7    CHANNEL_GROUP  CHARACTER VARYING(20)  0.000000e+00  APP        XFR           0         0     27
+           ATTNAME            FORMAT_TYPE           AVG                  MIN                  MAX           SUM  MAX_LENGTH
+0     ADD_TYPE_SID                INTEGER  4.452741e+07                    0            300180001  1.202240e+09         NaN
+1         ADD_TYPE  CHARACTER VARYING(20)           NaN                  NaN                  NaN           NaN        20.0
+2    ADD_TYPE_DESC  CHARACTER VARYING(50)           NaN                  NaN                  NaN           NaN        20.0
+3    ETL_LOAD_DATE              TIMESTAMP           NaN  2015-05-28 15:53:31  2022-06-07 06:19:30           NaN         NaN
+4  ETL_UPDATE_DATE              TIMESTAMP           NaN  2015-05-28 15:53:31  2022-06-07 06:19:30           NaN         NaN
+5      ETL_USER_ID  CHARACTER VARYING(20)           NaN                  NaN                  NaN           NaN        20.0
+6          CHANNEL  CHARACTER VARYING(50)           NaN                  NaN                  NaN           NaN        20.0
+7    CHANNEL_GROUP  CHARACTER VARYING(20)           NaN                  NaN                  NaN           NaN        20.0
 ```
--- src/main.py
+++ src/main.py
@@ -1,22 +1,80 @@
+################################################ Migration Validation Script ###################################################################
 # code to validate data migration from netezza to snowflake using snowflake Stored procedures and Netezza queries
 # uses snowflake connector and netezza connector libraries
+# author: Chins Kuriakose
+# Created on: 25/11/2022
+# Last updated on: 25/11/2022
 
 import json
 import nzpy
 import pandas as pd
+import numpy as np
 import snowflake.connector as sf
 
 
+class CountValidationError(Exception):
+    """
+        :descrption: Exception raised when the snowflake data count and netezza data count mismatches after the migration task.
+    """
+    def __init__(self, sf_count, net_count) -> None:
+        """
+            :description: constructor function to update the error message in the exception
+            :param sf_count: Count of records in snowflake db after migration.
+            :param net_count: Count of records in netezza db which was migrated to snowflake.
+            :usage: raise CountVaildationError(<sf_count>, <net_count>)
+            :output: Counts mismatched.
+                     Snowflake count : <sf_count>
+                     Netezza count : <net_count>
+        """
+        self.message = f"Counts mismatched.\nSnowflake count : {sf_count}\nNetezza count : {net_count}"
+        super().__init__(self.message)
+
+
+class DataValidationError(Exception):
+    """
+        :descrption: Exception raised when the Data validation between snowflake and netezza databases fail after the migration task.
+    """
+    def __init__(self, report: dict) -> None:
+        """
+            :description: constructor function to update the error message in the exception
+            :param report: Dictionary containing info regarding mismatched validation data
+            :usage: raise DataVaildationError(<report>)
+            :output: Data Validation Failed.
+                     Report:
+                     {
+                        "ADD_TYPE": {
+                            "MAX_LENGTH": {
+                            "SF": 22,
+                            "Netezza": 20.0
+                            }
+                        },
+                        "ETL_UPDATE_DATE": {
+                            "MIN": {
+                                "SF": "2015-05-28 15:53:00",
+                                "Netezza": "2015-05-28 15:53:31"
+                            }
+                        }
+                     }
+        """
+        self.message = f"Data Validation Failed.\nReport:\n{json.dumps(report, indent=4)}"
+        super().__init__(self.message)
+
+
 class Netezza:
-    def __init__(self, db_name, schema_name, table_name):
+    def __init__(self, db_name, schema_name, table_name, date_col=None, start_date=None, end_date=None):
         self.conn = None
         self.db_name = db_name
         self.schema_name = schema_name
         self.table_name = table_name
         self.full_table_name = f"{self.db_name}.{self.schema_name}.{self.table_name}"
+        self.date_col = date_col
+        self.start_date = start_date
+        self.end_date = end_date
+        self.where_clause = ''
         self.val_df = None
         # self.connect_netezza()
         self.get_column_dtype_info()
+        self.get_table_count()
         self.validate_columns()
 
     def connect_netezza(self):
@@ -26,6 +84,26 @@ class Netezza:
         """
         self.conn = nzpy.connect(user="admin", password="password", host='localhost',
                                  port=5480, database="db1", securityLevel=1, logLevel=0)
+
+    def get_table_count(self):
+        
+        self.where_clause = ""
+
+        if self.date_col:
+            if self.start_date and self.end_date:
+                self.where_clause = f"""\nwhere {self.date_col} > '{self.start_date}'
+                    and {self.date_col} < '{self.end_date}'"""
+            elif self.start_date:
+                self.where_clause = f"""\nwhere {self.date_col} > '{self.start_date}'"""
+            elif self.end_date:
+                self.where_clause = f"""\nwhere {self.date_col} < '{self.end_date}'"""
+
+        query = f"""select count(*)
+        from {self.full_table_name}{self.where_clause}"""
+
+        # self.table_count = pd.read_sql(query, conn) # use this line once the connection to netezza is created.
+        # remove this once connection to netezza is established
+        self.table_count = 27
 
     def get_column_dtype_info(self):
         query = f"""select 
@@ -39,16 +117,13 @@ class Netezza:
         # remove this once connection to netezza is established
         self.val_df = pd.read_csv(
             'src/netezza_col_dtype_sample.csv', sep=',', header='infer')
-        
 
     def int_col_checks(self, col):
         query = f"""select AVG({col}) as AVG, 
             MIN({col}) as MIN, 
             MAX({col}) as MAX, 
-            SUM({col}) as SUM, 
-            SUM(case when {col} is null then 1 else 0 end) as NULL_SUM, 
-            COUNT({col}) as COUNT
-        from {self.full_table_name}"""
+            SUM({col}) as SUM
+        from {self.full_table_name}{self.where_clause}"""
         # df = pd.read_sql(query, self.conn) # use this line once the connection to netezza is created
         # remove this once connection to netezza is established
         df = pd.read_csv('src/int_col_check_sample.csv')
@@ -56,21 +131,34 @@ class Netezza:
             df['MIN'][0], \
             df['MAX'][0], \
             df['SUM'][0], \
-            df['NULL_SUM'][0], \
-            df['COUNT'][0]
+            np.NaN
 
     def varchar_col_checks(self, col):
-        query = f"""select 0 as AVG, MIN({col}) as MIN, MAX({col}) as MAX, 0 as SUM, SUM(case when {col} is null then 1 else 0 end) as NULL_SUM, COUNT({col}) as COUNT
-        from {self.full_table_name}"""
+        query = f"""select 
+            max(length({col})) as MAX_LENGTH
+        from {self.full_table_name}{self.where_clause}"""
         # df = pd.read_sql(query, self.conn) # use this line once the connection to netezza is created
         # remove this once connection to netezza is established
         df = pd.read_csv('src/varchar_col_check_sample.csv')
-        return df['AVG'][0], \
+        return np.NaN, \
+            np.NaN, \
+            np.NaN, \
+            np.NaN, \
+            df['MAX_LENGTH'][0]
+
+    def datetime_col_checks(self, col):
+        query = f"""select 
+            min({col}) as MIN,
+            max({col}) as MAX
+        from {self.full_table_name}{self.where_clause}"""
+        # df = pd.read_sql(query, self.conn) # use this line once the connection to netezza is created
+        # remove this once connection to netezza is established
+        df = pd.read_csv('src/date_col_check_sample.csv')
+        return np.NaN, \
             df['MIN'][0], \
             df['MAX'][0], \
-            df['SUM'][0], \
-            df['NULL_SUM'][0], \
-            df['COUNT'][0]
+            np.NaN, \
+            np.NaN
 
     def func_selector(self, col):
         # for col in self.val_df['ATTNAME'].values:
@@ -80,16 +168,17 @@ class Netezza:
             return self.int_col_checks(col)
         elif 'CHAR' in dtype:
             return self.varchar_col_checks(col)
+        elif dtype in ['DATE', 'INTERVAL'] or 'TIME' in dtype:
+            return self.datetime_col_checks(col)
         else:
-            return 0, 0, 0, 0, 0, 0
+            return np.NaN, np.NaN, np.NaN, np.NaN, np.NaN
 
     def validate_columns(self):
         self.val_df['AVG'], \
             self.val_df['MIN'], \
             self.val_df['MAX'], \
             self.val_df['SUM'], \
-            self.val_df['NULL_SUM'], \
-            self.val_df['COUNT'] = zip(
+            self.val_df['MAX_LENGTH'] = zip(
                 *self.val_df['ATTNAME'].apply(self.func_selector))
         # print(self.val_df)
 
@@ -103,8 +192,11 @@ class Snowflake:
         self.schema_name = schema_name
         self.table_name = table_name
         self.full_table_name = f"{self.db_name}.{self.schema_name}.{self.table_name}"
+        self.table_count = None
+        self.val_json = dict()
         # self.connect_snowflake()
         self.get_validation_json()
+        self.get_table_count()
 
     def connect_snowflake(self):
         self.conn = sf.connect(
@@ -125,104 +217,40 @@ class Snowflake:
             self.val_json = json.load(f)
 
         # {col1 :{datatype: number, avg: 1, min : 2, max : 3}, clm_nm2 :varchar , lenth : ...}
-
-
-def data_validation(netezza, snowflake):
-
-    validation_df = pd.DataFrame()
-
-    for col in snowflake.val_json.keys():
-        s_avg = snowflake.val_json[col]['AVG'] if 'AVG' in snowflake.val_json[col].keys(
-        ) else 0
-        s_min = snowflake.val_json[col]['MIN'] if 'MIN' in snowflake.val_json[col].keys(
-        ) else 0
-        s_max = snowflake.val_json[col]['MAX'] if 'MAX' in snowflake.val_json[col].keys(
-        ) else 0
-        s_sum = snowflake.val_json[col]['SUM'] if 'SUM' in snowflake.val_json[col].keys(
-        ) else 0
-        s_nullsum = snowflake.val_json[col]['NULL_SUM'] if 'NULL_SUM' in snowflake.val_json[col].keys(
-        ) else 0
-        s_count = snowflake.val_json[col]['COUNT'] if 'COUNT' in snowflake.val_json[col].keys(
-        ) else 0
+    
+    def get_table_count(self):
+        self.table_count = self.val_json.pop('COUNT')
 
 
 
-        # print(col, 'AVG', netezza.val_df.loc[netezza.val_df['ATTNAME'] == col, 'AVG'].iloc[0])
-        n_avg = netezza.val_df.loc[netezza.val_df['ATTNAME'] == col, 'AVG'].iloc[0]
+def count_validation(netezza: Netezza, snowflake:Snowflake):
+    if netezza.table_count != snowflake.table_count:
+        raise CountValidationError(snowflake.table_count, netezza.table_count)
 
-        # print(col, 'MIN', netezza.val_df.loc[netezza.val_df['ATTNAME'] == col, 'MIN'].iloc[0])
-        n_min = netezza.val_df.loc[netezza.val_df['ATTNAME'] == col, 'MIN'].iloc[0]
 
-        # print(col, 'MAX', netezza.val_df.loc[netezza.val_df['ATTNAME'] == col, 'MAX'].iloc[0])
-        n_max = netezza.val_df.loc[netezza.val_df['ATTNAME'] == col, 'MAX'].iloc[0]
+def data_validation(netezza:Netezza, snowflake:Snowflake):
 
-        # print(col, 'SUM', netezza.val_df.loc[netezza.val_df['ATTNAME'] == col, 'SUM'].iloc[0])
-        n_sum = netezza.val_df.loc[netezza.val_df['ATTNAME'] == col, 'SUM'].iloc[0]
+    report = dict()
+    for col in netezza.val_df['ATTNAME'].values:
+        # for key in snowflake.val_json.keys():
+            params = list(snowflake.val_json[col].keys())
+            params.remove('DATATYPE')
+            for param in params:
+                if snowflake.val_json[col][param] != netezza.val_df.loc[netezza.val_df['ATTNAME'] == col, param].iloc[0]:
+                    if col not in report.keys():
+                        report[col] = dict()
+                    if param not in report[col].keys():
+                        report[col][param] = dict()
+                    report[col][param]['SF'] = snowflake.val_json[col][param]
+                    report[col][param]['Netezza'] = netezza.val_df.loc[netezza.val_df['ATTNAME'] == col, param].iloc[0]
 
-        # print(col, 'NULL_SUM', netezza.val_df.loc[netezza.val_df['ATTNAME'] == col, 'NULL_SUM'].iloc[0])
-        n_nullsum = netezza.val_df.loc[netezza.val_df['ATTNAME'] == col, 'NULL_SUM'].iloc[0]
-
-        # print(col, 'COUNT', netezza.val_df.loc[netezza.val_df['ATTNAME'] == col, 'COUNT'].iloc[0])  
-        n_count = netezza.val_df.loc[netezza.val_df['ATTNAME'] == col, 'COUNT'].iloc[0]      
-        
-        if not (s_avg == n_avg and s_min == n_min and s_max == n_max and s_sum == n_sum and s_nullsum == n_nullsum and s_count == n_count):
-            if validation_df.empty:
-                validation_df = pd.DataFrame(
-                    {
-                        'COLUMN_NM': [col],
-                        'DATATYPE': [snowflake.val_json[col]['DATATYPE']],
-                        'SF_AVG': [s_avg],
-                        'Net_AVG': [n_avg],
-                        'SF_MIN': [s_min],
-                        'Net_MIN': [n_min],
-                        'SF_MAX': [s_max],
-                        'Net_MAX': [n_max],
-                        'SF_SUM': [s_sum],
-                        'Net_SUM': [n_sum],
-                        'SF_NULLSUM': [s_nullsum],
-                        'Net_NULLSUM': [n_nullsum],
-                        'SF_COUNT': [s_count],
-                        'Net_COUNT': [n_count]
-                    }
-                )
-            else:
-                validation_df = pd.concat([
-                    validation_df,
-                    pd.DataFrame(
-                        {
-                            'COLUMN_NM': [col],
-                            'DATATYPE': [snowflake.val_json[col]['DATATYPE']],
-                            'SF_AVG': [s_avg],
-                            'Net_AVG': [n_avg],
-                            'SF_MIN': [s_min],
-                            'Net_MIN': [n_min],
-                            'SF_MAX': [s_max],
-                            'Net_MAX': [n_max],
-                            'SF_SUM': [s_sum],
-                            'Net_SUM': [n_sum],
-                            'SF_NULLSUM': [s_nullsum],
-                            'Net_NULLSUM': [n_nullsum],
-                            'SF_COUNT': [s_count],
-                            'Net_COUNT': [n_count]
-                        }
-                    )
-                ])
-
-    if validation_df.empty:
-        return True, None
-    else:
-        return False, validation_df
-    # return true|false based on validation
+    if report:
+        raise DataValidationError(report)
 
 
 if __name__ == '__main__':
     netezza = Netezza('DB', 'SCHEMA', 'TABLE')
 
     snowflake = Snowflake('DB', 'SCHEMA', 'TABLE')
-    validation_status, validation_df = data_validation(netezza, snowflake)
-    if validation_status:
-        print("Validation successfull")
-    else:
-        print("Validation Failed")
-        print("Here is the report : ")
-        print("\n", validation_df.to_string())
+    count_validation(netezza, snowflake)
+    data_validation(netezza, snowflake)
